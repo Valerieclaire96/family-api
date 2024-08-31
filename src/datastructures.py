a908753caf0a1c098d1e7b26151d77ce410c4b45
Valerie Dubach <116690000+Valerieclaire96@users.noreply.github.com>
2024-08-31 15:01:38 +0000

    works now

--- src/datastructures.py
+++ src/datastructures.py
@@ -19,13 +19,13 @@ class FamilyStructure:
     #     print(self.last_name)
     # read-only: Use this method to generate random members ID's when adding members into the list
     
-    # def _generateId(self):
-    #     return randint(0, 99999999)
+    def _generateId(self):
+        return randint(0, 99999999)
 
     def add_member(self, member):
         # Add a member to the members list
         if "id" not in member:
-            member["id"] = self._generate_id()
+            member["id"] = self._generateId()
         self._members.append(member)
         return member  # Returning the added member can be useful
 
@@ -33,14 +33,8 @@ class FamilyStructure:
     def delete_member(self, id):
         # fill this method and update the return
         self._members = list(filter(lambda member: member["id"] != id, self._members))
-        # new_family= []
-        # for member in self._members:
-        #     if member['id'] != id:
-        #         new_family.push(member) 
-        # self._members = new_family
         
-
-     def get_member(self, id):
+    def get_member(self, id):
         # Return a member from the members list by id
         for member in self._members:
             if member["id"] == id:
